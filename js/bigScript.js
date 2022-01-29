120a22964d910b9ab280c671a2f6199bcd173714
adityaShekhar <shekharaditya7@gmail.com>
2022-01-29 12:40:01 +0530

    function declarations are block scoped

--- js/bigScript.js
+++ js/bigScript.js
@@ -1,3 +1,11 @@
+// "use strict";
+{
+  function InBLock() {
+    console.log("Try to invoke me outside this block");
+  }
+}
+InBLock();
+
 const t = 200000;
 for (let i = 0; i < t; i++) {
   // uncomment to see how 'console logs' slow down things
