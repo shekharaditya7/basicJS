4ca62ab3a7b2dab0043318554a3e2c568bf22171
adityaShekhar <shekharaditya7@gmail.com>
2022-07-26 01:05:31 +0530

    Filters remaining

--- js/todo.js
+++ js/todo.js
@@ -1,15 +1,44 @@
-let idCount = 0;
+import todoAPI from "./todoAPI";
+let clientTodoList = [];
+const TASK_TYPES = {
+  ALL: "all",
+  ACTIVE: "active",
+  COMPLETED: "completed",
+};
 
-function createTotdoItem(title) {
-  idCount++;
+let currentTaskType = TASK_TYPES.ALL;
+
+const addButton = document.getElementById("addButton");
+const inputBox = document.getElementById("inputBox");
+const allButton = document.getElementById("allButton");
+const activeButton = document.getElementById("activeButton");
+const completedButton = document.getElementById("completedButton");
+const todoContainer = document.getElementById("todoList");
+
+async function handleAddTodo() {
+  const title = inputBox.value;
+  if (!title) return;
+
+  const todoData = await todoAPI.createTodo({ title });
+  todoContainer.innerHTML = "";
+  todoData?.data?.todoList?.forEach((element) => {
+    const todoElement = createTotdoElement(element);
+    todoContainer.appendChild(todoElement);
+  });
+  inputBox.value = "";
+}
+
+function createTotdoElement({ id, title, isCompleted }) {
   const item = document.createElement("li");
-  item.id = idCount;
+  item.id = id;
 
   const titleElement = document.createElement("h2");
   titleElement.innerText = title;
 
   const isCompletedElement = document.createElement("input");
   isCompletedElement.type = "checkbox";
+  isCompletedElement.checked = isCompleted;
+  isCompletedElement.disabled = true;
 
   const editElement = document.createElement("button");
   editElement.innerText = "Edit";
@@ -20,20 +49,31 @@ function createTotdoItem(title) {
     editBox.style.display = "block";
     editBox.value = title;
     titleElement.replaceWith(editBox);
+    isCompletedElement.disabled = false;
 
-    editElement.addEventListener("click", (event) => {
-      const newTitle = editBox.value;
-      const newTitleElement = document.createElement("h2");
-      newTitleElement.innerText = newTitle;
-      editElement.innerText = "Edit";
-      editBox.replaceWith(newTitleElement);
+    editElement.addEventListener("click", async (event) => {
+      const todoData = await todoAPI.updateTodo({
+        id,
+        title: editBox.value,
+        isCompleted: isCompletedElement.checked,
+      });
+      todoContainer.innerHTML = "";
+      todoData?.data?.todoList?.forEach((element) => {
+        const todoElement = createTotdoElement(element);
+        todoContainer.appendChild(todoElement);
+      });
     });
   });
 
   const deleteElement = document.createElement("button");
   deleteElement.innerText = document.innerText = "Delete";
-  deleteElement.addEventListener("click", (event) => {
-    todoContainer.removeChild(item);
+  deleteElement.addEventListener("click", async (event) => {
+    const todoData = await todoAPI.deleteTodo({ id });
+    todoContainer.innerHTML = "";
+    todoData?.data?.todoList?.forEach((element) => {
+      const todoElement = createTotdoElement(element);
+      todoContainer.appendChild(todoElement);
+    });
   });
 
   item.appendChild(titleElement);
@@ -44,20 +84,20 @@ function createTotdoItem(title) {
   return item;
 }
 
-const addButton = document.getElementById("addButton");
-const inputBox = document.getElementById("inputBox");
-const todoContainer = document.getElementById("todoList");
-
-const handleAddTodo = (event) => {
-  todoTitle = inputBox.value;
-  if (!todoTitle) return;
-
-  const todoItem = createTotdoItem(todoTitle);
-  inputBox.value = "";
-  todoContainer.appendChild(todoItem);
-};
-
 addButton.addEventListener("click", handleAddTodo);
 inputBox.addEventListener("keypress", (event) => {
-  if (event.code == "Enter") handleAddTodo(event);
+  if (event.code == "Enter") handleAddTodo();
+});
+
+allButton.addEventListener("click", () => {
+  currentTaskType = TASK_TYPES.ALL;
+});
+
+window.addEventListener("load", async () => {
+  const todoData = await todoAPI.getTodos();
+  clientTodoList = todoData?.data?.todoList || [];
+  todoData?.data?.todoList?.forEach((element) => {
+    const todoElement = createTotdoElement(element);
+    todoContainer.appendChild(todoElement);
+  });
 });
---
+++ js/todoAPI.js
@@ -0,0 +1,61 @@
+import { nanoid } from "nanoid";
+const TODO_KEY = "todoList";
+const DELAY = 1000;
+
+function getTodos() {
+  const todoList = JSON.parse(localStorage.getItem(TODO_KEY)) || [];
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({ data: { todoList } });
+    }, DELAY);
+  });
+}
+
+function createTodo({ title }) {
+  if (!title) throw new Error("Title not provided");
+  const id = nanoid();
+  const todoItem = { id, title, isCompleted: false };
+  const prevTodoItems = JSON.parse(localStorage.getItem(TODO_KEY)) || [];
+  const newTodoList = [todoItem, ...prevTodoItems];
+  localStorage.setItem(TODO_KEY, JSON.stringify(newTodoList));
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({ data: { todoList: newTodoList } });
+    }, DELAY);
+  });
+}
+
+function deleteTodo({ id }) {
+  if (!id) throw new Error("Id not Provided");
+  const todoList = JSON.parse(localStorage.getItem(TODO_KEY)) || [];
+  const indexToBeDeleted = todoList.findIndex((item) => item.id === id);
+  if (indexToBeDeleted === -1) throw new Error("Id does not exist");
+  todoList.splice(indexToBeDeleted, 1);
+  localStorage.setItem(TODO_KEY, JSON.stringify(todoList));
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({ data: { todoList } });
+    }, DELAY);
+  });
+}
+
+function updateTodo({ id, isCompleted, title }) {
+  if (!id) throw new Error("Id not Provided");
+  if (typeof isCompleted !== "boolean")
+    throw new Error("Invalid Status Provided");
+  if (!title) throw new Error("Empty Title Provided");
+
+  const todoList = JSON.parse(localStorage.getItem(TODO_KEY)) || [];
+  const indexToBeUpdated = todoList.findIndex((item) => item.id === id);
+  if (indexToBeUpdated === -1) throw new Error("Id does not exist");
+  const todoItem = { id, title, isCompleted };
+  todoList[indexToBeUpdated] = todoItem;
+  localStorage.setItem(TODO_KEY, JSON.stringify(todoList));
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({ data: { todoList } });
+    }, DELAY);
+  });
+}
+
+export default { getTodos, createTodo, deleteTodo, updateTodo };
